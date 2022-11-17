458e3a8b3a45135fd4b211183cc965393d252007
Bugra <bugra.gunay@softinyo.com>
2022-11-17 11:59:07 +0300

    fix_services

--- lib/services/search_services.dart
+++ lib/services/search_services.dart
@@ -1,5 +1,7 @@
 import 'package:assistanapp/core/constants/app_strings.dart';
 import 'package:assistanapp/models/exercises/exercises.dart';
+import 'package:flutter/rendering.dart';
+import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../core/constants/app_url.dart';
@@ -13,11 +15,15 @@ class SearchServices {
   //* Get Exercises Data Service
   static Future<List<Exercises>> getExercisesData({String? parameters}) async {
     Uri uri = Uri.parse('${AppUrls.url}${parameters ?? ''}');
-    final response = await http.get(uri, headers: _headers);
-    if (response.statusCode == 200) {
-      return (jsonDecode(response.body) as List).map((l) => Exercises.fromJson(l)).toList();
-    } else {
-      throw Exception('Failed to load json data');
+    try {
+      final response = await http.get(uri, headers: _headers);
+      if (response.statusCode == 200) {
+        return (jsonDecode(response.body) as List).map((l) => Exercises.fromJson(l)).toList();
+      } else {
+        throw Exception('Failed to load json data');
+      }
+    } catch (e) {
+      throw Exception('Server Internal Error');
     }
   }
 }
